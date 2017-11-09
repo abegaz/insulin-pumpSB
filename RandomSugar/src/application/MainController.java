1dec1f02c021e37bf2b04a679d905cbdd8ea1659
Henry Garcia <henrygarcia937@gmail.com>
2017-11-09 14:23:03 -0500

    Update MainController.java

--- RandomSugar/src/application/MainController.java
+++ RandomSugar/src/application/MainController.java
@@ -1,156 +1,102 @@
 package application;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.animation.TranslateTransition;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+
+
 public class MainController {
+    @FXML
+    private Button awesome;
+    @FXML
+    private AnchorPane navList;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button consumeSugar;
+	@FXML
+    private Label myNum1;
+    @FXML
+    private AnchorPane navList2;
+    @FXML
+    private Button exitButton2;
+    @FXML
+    private Button insulinReservoir;
 
 
-	    @FXML
-	    private Button awesome;
-	    @FXML
-	    private AnchorPane navList;
-	    @FXML
-	    private Button exitButton;
-	    @FXML
-	    private Button consumeSugar;
-	    @FXML
-	    private AnchorPane navList2;
-	    @FXML
-	    private Button exitButton2;
-	    @FXML
-	    private Button insulinReservoir;
-	    @FXML
-	    private Label myNum;
-	    @FXML
-	    private Label myNum1;
 
 
-	String url = "jdbc:mysql://localhost:3306/demo";
-	String user = "root";
-	String password = "";
-	Connection myConn = null;
-	Statement myStmt = null;
-	Timestamp ts = new Timestamp(System.currentTimeMillis());
 
-	public void generateRandom(ActionEvent event) {
+@FXML private Label myNum;
+
+public void generateRandom(ActionEvent event) {
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask(){
 			public void run(){
 				Platform.runLater(new Runnable(){ //What allows the UI to be modified
 					public void run(){
-						Random rand = new Random();
-						int bloodSugar = rand.nextInt(151)+ 50;
-						myNum.setText(Integer.toString(bloodSugar));
+						BloodSugar bs1;
+							bs1 = new BloodSugar();
+							bs1.random();
+							System.out.println(bs1.getBs());
+							bs1.uploadBSToDB();
+							myNum.setText(Integer.toString(bs1.getBs()));
+
+							TranslateTransition openNav=new TranslateTransition(new Duration(350), navList);
+					        openNav.setToY(0);
+					        TranslateTransition closeNav=new TranslateTransition(new Duration(350), navList);
+
+							if(bs1.getBs()  <= 100) {
+								navList.getTranslateY();
+					            openNav.play();
+					            } else {
+					                closeNav.setToY(-(navList.getHeight()));
+					                closeNav.play();
+					            }
 
 
-						//Animation for low blood sugar
-				        TranslateTransition openNav=new TranslateTransition(new Duration(650), navList);
-				        openNav.setToY(0);
-				        TranslateTransition closeNav=new TranslateTransition(new Duration(650), navList);
+							consumeSugar.setOnAction(new EventHandler<ActionEvent>() {
+							    @Override public void handle(ActionEvent e) {
 
-						if(bloodSugar  <= 100) {
-							navList.getTranslateY();
-				            openNav.play();
-				            } else {
-				                closeNav.setToY(-(navList.getHeight()));
-				                closeNav.play();
-				            }
-						// End of animation
-
-
-					    // Exit button to close alert box
-						exitButton.setOnAction(new EventHandler<ActionEvent>() {
-						    @Override public void handle(ActionEvent e) {
-						    	closeNav.setToY(-(navList.getHeight()));
-				                closeNav.play();
-						    }
-						});
-
-
-						try {
-							myConn = DriverManager.getConnection(url, user, password);
-							myStmt = myConn.createStatement();
-							String sql = "insert into info " + " (bloodSugar,time)"
-									+ " values ('"+bloodSugar+"', '"+ts+"')";
-							myStmt.executeUpdate(sql);
-							System.out.println("Insert complete.");
-							} catch (Exception exc) {
-								exc.printStackTrace();
-							} finally {
-								if (myStmt != null) {
-									try {
-										myStmt.close();
-									} catch (SQLException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-								}
-								if (myConn != null) {
-									try {
-										myConn.close();
-									} catch (SQLException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-								}
-
-					}
-						// Clicking 'Consume Sugar' will add 50
-						consumeSugar.setOnAction(new EventHandler<ActionEvent>() {
-						    @Override public void handle(ActionEvent e) {
-
-						    	myNum.setText(Integer.toString(bloodSugar + 50));
-						    	closeNav.setToY(-(navList.getHeight()));
-				                closeNav.play();
-						    }
-						});
-
-					}
+							    	myNum.setText(Integer.toString(bs1.getBs() + 50));
+							    	closeNav.setToY(-(navList.getHeight()));
+					                closeNav.play();
+							    }
+							});
+							exitButton.setOnAction(new EventHandler<ActionEvent>() {
+							    @Override public void handle(ActionEvent e) {
+							    	closeNav.setToY(-(navList.getHeight()));
+					                closeNav.play();
+							    }
+							});
+ }
 				});
-
-
-			}},0,20000); //,first parameter is the delay before the FIRST measurement is taken (milliseconds)
+			}},0,5000); //,first parameter is the delay before the FIRST measurement is taken (milliseconds)
 				   //second parameter is how long before the next measurement is taken (milliseconds)
 				   //second parameter set to 5 seconds (5000 milliseconds) for testing purposes
 				   //to set the timer for 15 minutes use either 15*60*1000 OR 900000 for the second parameter
 
 	}
 
-
-
-
-
-// Test code for testing insulin reservoir, creating alert box, rng # generator, and refill box
-
-
- public void generateInsulin(ActionEvent event) {
-
-	Random rand2 = new Random();
-	int insulinLevel = rand2.nextInt(131)+ 50;
+public void generateInsulin(ActionEvent event) {
+	int insulinLevel = (int)(Math.random()*151) + 50;
 	myNum1.setText(Integer.toString(insulinLevel));
 
 
-	//Animation for low insulin reservoir
-    TranslateTransition openNav2=new TranslateTransition(new Duration(650), navList2);
+	//Animation for low blood sugar
+    TranslateTransition openNav2=new TranslateTransition(new Duration(350), navList2);
     openNav2.setToY(0);
-    TranslateTransition closeNav2=new TranslateTransition(new Duration(650), navList2);
+    TranslateTransition closeNav2=new TranslateTransition(new Duration(350), navList2);
 
     if(insulinLevel  <= 100) {
 		navList2.getTranslateY();
@@ -179,4 +125,8 @@ public class MainController {
 
 }
 
+
+
 }
+
+
