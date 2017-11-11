551ad0a80ead091d0c0305a2825bc5b8af71ff6f
Henry Garcia <henrygarcia937@gmail.com>
2017-11-11 17:44:55 -0500

    Update Main.java

--- FirstInsulinPump/src/application/Main.java
+++ FirstInsulinPump/src/application/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 		});
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,500,500);
+			Scene scene = new Scene(root,1000,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
